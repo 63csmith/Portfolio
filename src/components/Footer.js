952173cee97d591f72c4d98529b985250136d97f
63csmith <63csmith@gmail.com>
2022-07-08 10:40:44 -0600

    another fix

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import { Container, Row, Col } from "react-bootstrap";
+import smith from '../assets/img/Smith-logos_white.png';
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          
+          <Col size={12} sm={6}>
+            <img src={smith} alt="Logo" />
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <p>Copyright 2022. All Rights Reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
